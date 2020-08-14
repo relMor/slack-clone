52feeab7201d459705017d22d0aa52cf3ef83542
relMor <amanvarma.600847@gmail.com>
2020-08-14 20:14:53 +0530

    completed header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>SLACK CLONE</h1>
+      <Header />
+      {/* sidebar */}
+      {/* router */}
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import Avatar from "@material-ui/core/Avatar";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="Aman Varma" src="" />
+        <AccessTimeIcon />
+        {/* Time icon */}
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search Clever Programmer" type="text" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
